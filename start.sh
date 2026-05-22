073a24adbb6081b27218e93abcfe907cd78f483a
weiyanmin <weiyanmin@users.noreply.github.com>
2026-05-22 14:47:44 +0700

    Fix python command in start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the Telegram bot in the background
-python -m bot.main &
+python3 -m bot.main &
 
 # Start the FastAPI server in the foreground
 uvicorn api.server:app --host 0.0.0.0 --port $PORT
